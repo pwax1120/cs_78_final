fdb4c8dcbf56bc5945a04270d5d2736b05b24539
pwax1120 <paul.a.waxman.27@dartmouth.edu>
2026-06-02 11:10:00 -0400

    Updated training fixed 1 -> 1.0

--- training.py
+++ training.py
@@ -2,7 +2,7 @@ import argparse
 from model_finetune import train, FinetuneConfig
 
 CONFIGS = [
-    '''
+    
     #CIFAR-10 FULL FINETUNE (5 seeds x 3 label fractions x 2 modes = 30 runs)
     FinetuneConfig(dataset="cifar10", num_classes=10, label_fraction=0.01, mode="full", lr=0.003125, momentum=0.9, weight_decay=0.0, optimizer="sgd", epochs=60, batch_size=256, scheduler="cosine", seed=0, device="auto", num_workers=0),
     FinetuneConfig(dataset="cifar10", num_classes=10, label_fraction=0.01, mode="full", lr=0.003125, momentum=0.9, weight_decay=0.0, optimizer="sgd", epochs=60, batch_size=256, scheduler="cosine", seed=1, device="auto", num_workers=0),
@@ -34,7 +34,7 @@ CONFIGS = [
     FinetuneConfig(dataset="cifar10", num_classes=10, label_fraction=1.0, mode="linear_eval", lr=0.003125, momentum=0.9, weight_decay=0.0, optimizer="sgd", epochs=60, batch_size=256, scheduler="cosine", seed=2, device="auto", num_workers=0),
     FinetuneConfig(dataset="cifar10", num_classes=10, label_fraction=1.0, mode="linear_eval", lr=0.003125, momentum=0.9, weight_decay=0.0, optimizer="sgd", epochs=60, batch_size=256, scheduler="cosine", seed=3, device="auto", num_workers=0),
     FinetuneConfig(dataset="cifar10", num_classes=10, label_fraction=1.0, mode="linear_eval", lr=0.003125, momentum=0.9, weight_decay=0.0, optimizer="sgd", epochs=60, batch_size=256, scheduler="cosine", seed=4, device="auto", num_workers=0),
-    '''
+    
     #EURO-SAT FULL FINETUNE (Same structure as cifar-10)
 
     FinetuneConfig(dataset="eurosat", num_classes=10, label_fraction=0.01, mode="full", lr=0.003125, momentum=0.9, weight_decay=0.0, optimizer="sgd", epochs=60, batch_size=256, scheduler="cosine", seed=0, device="auto", num_workers=0),
